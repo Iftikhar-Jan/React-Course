e6e91e3827dfc3ebf24a46ad7bd27370d8c1a967
Iftikhar-Jan <ijan709@gmail.com>
2024-09-30 16:03:23 +0500

    CoursE class 1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Hello</h1>
+    <button>Home</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export const Home = () => {
+  return(
+    <h1>Home</h1>
+  )
+}
+
+export const Contact = () => {
+  return(
+    <h1>Home</h1>
+  )
+}
+
+export const About = () => {
+  return(
+    <h1>Home</h1>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {App, Home, About, Contact} from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Home />
+    <About />
+    <Contact />
   </React.StrictMode>
 );
 
